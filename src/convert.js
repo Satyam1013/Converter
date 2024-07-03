46fda6c6c4839b235f35dca70ce2e954f7cffeac
Satyam Banwale <satyambanwale@prodios.com>
2024-07-03 17:56:56 +0530

    added some functions

--- src/convert.js
+++ src/convert.js
@@ -6,3 +6,15 @@ function Converter(data) {
 }
 
 Converter("667943d1e4ccb0218e445f26");
+
+function UppercaseChanger(value) {
+  console.log(value.toUpperCase())
+}
+
+UppercaseChanger('satyam')
+
+function NumberChanger(value) {
+  console.log(value%100)
+}
+
+NumberChanger(123)
